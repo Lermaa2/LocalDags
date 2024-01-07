b1ce199aae9694f6c1eaabfcf747f852169645cf
James <jaime.salazar.lerma@gmail.com>
2024-01-08 00:26:09 +0100

    Ss

---
+++ helloAiflow.py
@@ -0,0 +1,29 @@
+from datetime import timedelta
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.utils.dates import days_ago
+
+def print_hello():
+    return 'Hello world from Airflow!'
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': days_ago(0),
+    'email': ['your_email@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG(
+    'hello_world', 
+    default_args=default_args,
+    description='A simple hello world DAG',
+    schedule_interval=timedelta(days=1),
+)
+
+hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
+
+hello_operator
